fb1ec1851f5c05d36bc63bb3e5800c48a99e5f27
Jose Felipe Almeida dos Santos <josefelipedev@gmail.com>
2023-06-26 20:49:26 -0300

    Ajust

--- app/src/main/java/com/example/myapplication/MapFragment.java
+++ app/src/main/java/com/example/myapplication/MapFragment.java
@@ -1,10 +1,15 @@
 package com.example.myapplication;
 
+import android.Manifest;
 import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.preference.PreferenceManager;
 import android.widget.Toast;
 
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -22,15 +27,14 @@ import org.osmdroid.views.overlay.MapEventsOverlay;
 
 public class MapFragment extends Fragment {
 
+    private static final int REQUEST_LOCATION_PERMISSION = 1;
+
     private MapView map = null;
+    private IMapController mapController = null;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-
-
-
-
         Context ctx = getActivity().getApplicationContext();
         Configuration.getInstance().load(ctx, PreferenceManager.getDefaultSharedPreferences(ctx));
 
@@ -39,17 +43,13 @@ public class MapFragment extends Fragment {
         map = (MapView) view.findViewById(R.id.map);
         map.setTileSource(TileSourceFactory.MAPNIK);
 
-
-        GeoPoint shoppingLocation = new GeoPoint(-26.9875, -48.6350);
-        IMapController mapController = map.getController();
+        mapController = map.getController();
         mapController.setZoom(19.5);
-        mapController.setCenter(shoppingLocation);
 
-
-        MapEventsReceiver mReceive = new MapEventsReceiver(){
+        MapEventsReceiver mReceive = new MapEventsReceiver() {
             @Override
             public boolean singleTapConfirmedHelper(GeoPoint p) {
-                Toast.makeText(getActivity(), "Tap on ("+p.getLatitude()+","+p.getLongitude()+")",Toast.LENGTH_SHORT).show();
+                Toast.makeText(getActivity(), "Tap on (" + p.getLatitude() + "," + p.getLongitude() + ")", Toast.LENGTH_SHORT).show();
 
                 Marker startMarker = new Marker(map);
                 startMarker.setPosition(p);
@@ -57,6 +57,7 @@ public class MapFragment extends Fragment {
                 map.getOverlays().add(startMarker);
                 return false;
             }
+
             @Override
             public boolean longPressHelper(GeoPoint p) {
                 return false;
@@ -65,13 +66,48 @@ public class MapFragment extends Fragment {
         MapEventsOverlay OverlayEvents = new MapEventsOverlay(getActivity().getBaseContext(), mReceive);
         map.getOverlays().add(OverlayEvents);
 
-
         map.setUseDataConnection(true);
         map.setTileSource(TileSourceFactory.DEFAULT_TILE_SOURCE);
+
+        printCurrentLocation();
 
         return view;
     }
 
+    private void requestLocationPermission() {
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION_PERMISSION);
+            return;
+        }
+    }
+
+    private void printCurrentLocation() {
+        LocationManager locationManager = (LocationManager) getActivity().getSystemService(Context.LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestLocationPermission();
+            return;
+        }
+
+        Location location = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+        if (location != null) {
+            double latitude = location.getLatitude();
+            double longitude = location.getLongitude();
+
+            // Use latitude / longitude
+            Toast.makeText(getActivity(), "Localização atual: (" + latitude + "," + longitude + ")", Toast.LENGTH_SHORT).show();
+
+            // Move the map to the user's location
+            GeoPoint userLocation = new GeoPoint(latitude, longitude);
+            mapController.setCenter(userLocation);
+        }
+    }
 
     @Override
     public void onResume() {
@@ -85,3 +121,4 @@ public class MapFragment extends Fragment {
         map.onPause();
     }
 }
+
